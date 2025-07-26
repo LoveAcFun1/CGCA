a6104e2adfa3bef4aab4799c2caddd023f04590f
LoveAcFun1 <50582711+LoveAcFun1@users.noreply.github.com>
2025-07-26 11:48:04 +0800

    Delete scripts/test.sh

--- scripts/test.sh
+++
@@ -1,7 +0,0 @@
-
-# ASCEND_RT_VISIBLE_DEVICES=0 python test_qlora.py --train_args_file train_args/qlora/llama2-sft-qlora.json \
-# &> logs/llama2-test.log
-
-ASCEND_RT_VISIBLE_DEVICES=5 python test_qlora.py --train_args_file train_args/qlora/Qwen2.5-sft-qlora.json \
-&> logs/Qwen2.5-test.log
-
